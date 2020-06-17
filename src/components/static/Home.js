cfb69a204fc7c13f4bc6cf4c23a8c82cc719cde5
SACHIN RAJ <sacchinn619@gmail.com>
2020-06-17 17:16:53 +0530

    home

--- src/components/static/Home.js
+++ src/components/static/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Home(props){
     return(
         <div>
-            <h1>Welcome to ticket master App</h1>
+            <h1>Welcome to ticket master App!!</h1>
             <img src='https://www.virtuenetz.com/wp-content/uploads/2016/03/AGENT-INSIGHT-5-1200x700.png'alt=""/>
             </div>
     )
